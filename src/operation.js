6d23f1371bbbf8f7d5f79fa24cf60aa33605de55
Nerond32 <nerond32@gmail.com>
2019-07-27 13:14:51 +0200

    then and catch

--- src/operation.js
+++ src/operation.js
@@ -66,31 +66,39 @@ function Operation() {
     successReactions: [],
     failureReactions: []
   };
-  operation.onCompletion = (onSuccess, onError) => {
+  operation.then = (onSuccess, onError) => {
     const completionOp = new Operation();
     const successHandler = () => {
       if (onSuccess) {
         const callbackResult = onSuccess(operation.result);
-        if (callbackResult && callbackResult.onCompletion) {
+        if (callbackResult && callbackResult.then) {
           callbackResult.forwardCompletion(completionOp);
         }
+      }
+    };
+    const errorHandler = () => {
+      if (onError) {
+        const callbackResult = onError(operation.error);
+        completionOp.succeed(callbackResult);
       }
     };
     if (operation.state == 'succeeded') {
       successHandler();
     } else if (operation.state == 'failed') {
-      onError(operation.err);
+      errorHandler();
     } else {
       operation.successReactions.push(successHandler);
       if (onError) {
-        operation.failureReactions.push(onError);
+        operation.failureReactions.push(errorHandler);
       }
     }
     return completionOp;
   };
-  operation.onFailure = onError => {
-    return operation.onCompletion(null, onError);
+
+  operation.catch = onError => {
+    return operation.then(null, onError);
   };
+
   operation.succeed = result => {
     operation.state = 'succeeded';
     operation.result = result;
@@ -113,26 +121,23 @@ function Operation() {
     operation.succeed(result);
   };
   operation.forwardCompletion = op => {
-    operation.onCompletion(op.succeed, op.fail);
+    operation.then(op.succeed, op.fail);
   };
   return operation;
 }
 
 test('unnesting', done => {
-  let weatherOp = fetchCurrentCity().onCompletion(city => {
-    return fetchWeather(city);
-  });
-  weatherOp.onCompletion(weather => {
-    done();
-  });
+  fetchCurrentCity()
+    .then(city => fetchWeather(city))
+    .then(weather => done());
 });
 
 test('lexical paralleism', done => {
   const city = 'NYC';
   const weatherOp = fetchWeather(city);
   const forecastOp = fetchForecast(city);
-  weatherOp.onCompletion(weather => {
-    forecastOp.onCompletion(forecast => {
+  weatherOp.then(weather => {
+    forecastOp.then(forecast => {
       done();
     });
   });
@@ -140,24 +145,41 @@ test('lexical paralleism', done => {
 
 test('register only success handler, ignores error', done => {
   const operation = fetchCurrentCity();
-  operation.onFailure(error => done(error));
-  operation.onCompletion(result => done(), null);
+  operation.catch(error => done(error));
+  operation.then(result => done(), null);
 });
 
 test('register only error handler, ignores success', done => {
   const operation = fetchWeather();
-  operation.onCompletion(result => done(new Error("shouldn't succeed")), null);
-  operation.onFailure(error => done());
+  operation.then(result => done(new Error("shouldn't succeed")), null);
+  operation.catch(error => done());
 });
 
 test('pass multiple callbacks -- all of them called', done => {
   const operation = fetchCurrentCity();
   const multiDone = callDone(done).afterTwoCalls();
-  operation.onCompletion(result => multiDone(), null);
-  operation.onCompletion(result => multiDone(), null);
+  operation.then(result => multiDone(), null);
+  operation.then(result => multiDone(), null);
 });
 
 test('fetchCurrentCity pass the callbacks later on', done => {
   const operation = fetchCurrentCity();
-  operation.onCompletion(result => done(), null);
+  operation.then(result => done(), null);
+});
+
+const fetchCurrentCityThatFails = () => {
+  let operation = new Operation();
+  doLater(() => operation.fail('GPS BROKEN'));
+  return operation;
+};
+
+test('error recovery', done => {
+  fetchCurrentCityThatFails()
+    .catch(() => {
+      return 'default city';
+    })
+    .then(city => {
+      expect(city).toBe('default city');
+      done();
+    });
 });
